69baee43d98895d84a4861dd57842c750d212a1e
gracea18 <72771341+gracea18@users.noreply.github.com>
2020-11-21 00:08:29 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 tempFi = input.temperature(TemperatureUnit.FAHRENHEIT)
-tempF= 78
+tempF= 50
 while True:
     print("Temperature" + input.temperature(TemperatureUnit.FAHRENHEIT))
     #if tempF > 60:
     #light.set_pixel_color(5, light.rgb (255, 0 ,0))
 
-    if tempF > 70:
-        light.set_pixel_color(5, light.rgb (255, 0, 0))
-    elif tempF > 40:
-        light.set_pixel_color(5, light.rgb (0, 255, 0))
-    else:
-        light.set_pixel_color(5, light.rgb (0, 0, 255))
+    #if tempF > 70:
+        #light.set_pixel_color(5, light.rgb (255, 0, 0))
+    #elif tempF > 40:
+        #light.set_pixel_color(5, light.rgb (0, 255, 0))
+    #else:
+        #light.set_pixel_color(5, light.rgb (0, 0, 255))
 
-    #if tempF 
+    if tempF > 70: 
+        light.set_all(light.rgb (255,0,0))
+    elif tempF > 40:
+        light.set_all(light.rgb(0,255,0))
+    else:
+        light.set_all(light.rgb (0,0,255)) 
